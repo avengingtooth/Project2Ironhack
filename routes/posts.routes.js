918946435754c87a85a885021dccccef69c736db
Sebastian Mottschall <srophkor@gmail.com>
2023-02-21 17:30:14 +0100

    follow/like buttons toggle correctly when used, also rewrote posts slightly to accomodate follows/likes and use custom helper for identifyig author

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -5,12 +5,23 @@ const Post = require('../models/Post.model');
 const isPostAuthor = require('../middleware/isPostAuthor.js');
 const isLoggedIn = require('../middleware/isLoggedIn.js');
 const postData = require('../public/js/postData.js');
+const Follow = require('../models/Follow.model.js');
+const PostLike = require('../models/PostLike.model.js');
 
 router.get('/all', async(req, res, next) => {
     try{
         let curVisiblePosts = await Post.find().populate('author tags')
-        console.log(curVisiblePosts[curVisiblePosts.length - 1])
-        res.render('post/feed', {curVisiblePosts})
+
+        // create arrays of all users the user follows, and of all posts the user liked
+        let follows = [];
+        let likes = [];
+        if (req.session.currentUser) {
+            follows = await Follow.find({follower: req.session.currentUser});
+            likes = await PostLike.find({user: req.session.currentUser});
+            likes = likes.map(elem => elem.post);
+            follows = follows.map(elem => elem.followedUser);
+        }
+        res.render('post/feed', {curVisiblePosts, follows, likes})
     }
     catch(error){
         console.log(error)
@@ -67,8 +78,21 @@ router.get('/:postId/delete', isLoggedIn, isPostAuthor, async(req, res, next) =>
 
 router.get('/:id', async(req, res, next) => {
     const curPost = await Post.findById(req.params.id).populate('author tags')
-    const correctUser = curPost.author._id.equals(req.session.currentUser._id)
-    res.render('post/onePost', {curPost, correctUser})
+
+    // NOTE: this functionality has been used to hbs helper function
+    // const correctUser = curPost.author._id.equals(req.session.currentUser._id)
+
+     // create arrays of all users the user follows, and of all posts the user liked
+     let follows = [];
+     let likes = [];
+     if (req.session.currentUser) {
+         follows = await Follow.find({follower: req.session.currentUser});
+         likes = await PostLike.find({user: req.session.currentUser});
+         likes = likes.map(elem => elem.post);
+         follows = follows.map(elem => elem.followedUser);
+     }
+
+    res.render('post/onePost', {curPost, follows, likes})
 })
 
 module.exports = router
