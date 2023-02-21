a6f951dae98d2d0b3bffffd5b872b38424fd1f65
Sebastian Mottschall <srophkor@gmail.com>
2023-02-21 21:16:19 +0100

    feeds for followed users and liked posts added

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,4 +1,12 @@
-module.exports = (req, res, next) => {
+ /**
+  * Middleware that checks whether the current request was sent by a user that is logged into the app or not.
+  * Only lets requests through that come from users that ARE logged in.
+  * @param {Request} req 
+  * @param {Response} res 
+  * @param {NextFunction} next 
+  * @returns 
+  */
+ function isLoggedIn (req, res, next) {
   // checks if the user is logged in when trying to access a specific page
   if (!req.session.currentUser) {
     return res.redirect("/auth/login");
@@ -6,3 +14,5 @@ module.exports = (req, res, next) => {
 
   next();
 };
+
+module.exports = isLoggedIn;
