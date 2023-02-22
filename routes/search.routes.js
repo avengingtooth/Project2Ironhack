cab37917171653ff8fea43fa3326e151113ab7d5
avengingtooth <esellin@gmail.com>
2023-02-22 10:04:52 +0100

    search queries appearing

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -1,14 +1,23 @@
 const express = require('express');
-const Post = require('../models/Post.model');
+const Post = require('../models/Post.model')
+const Follow = require('../models/Follow.model')
+const PostLike = require('../models/PostLike.model')
 const router = express.Router();
 
 router.use('/', async(req, res, next) => {
     const searchValue = req.query.search
-    console.log(searchValue)
-    const queryResults = await Post.find({title: searchValue})
-    console.log(queryResults)
-    console.log(queryResults, "hiii")
-    res.render('search/results')
+    const queryResults = await Post.find({title: searchValue}).populate('author tags')
+    let follows = [];
+    let likes = [];
+
+    if (req.session.currentUser) {
+        follows = await Follow.find({follower: req.session.currentUser});
+        likes = await PostLike.find({user: req.session.currentUser});
+        likes = likes.map(elem => elem.post);
+        follows = follows.map(elem => elem.followedUser);
+    }
+
+    res.render('search/results', {queryResults, follows, likes})
 })
 
 module.exports = router
