5de52deb8a60312747542b43c551b448720a8aae
avengingtooth <esellin@gmail.com>
2023-02-23 15:06:35 +0100

    styles for creation page

--- public/js/createTagInput.js
+++ public/js/createTagInput.js
@@ -7,5 +7,5 @@ document.querySelector('#newTagBtn').addEventListener("click", () => {
     const newInput = document.createElement('input')
     newInput.setAttribute('name', 'tag')
     newInput.placeholder = 'Tag'
-    document.getElementById('tagContainer').append(newInput)
+    document.querySelector('.allTags').append(newInput)
 })
